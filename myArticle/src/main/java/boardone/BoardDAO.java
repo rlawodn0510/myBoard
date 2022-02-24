9745076374577c8bc977bab577b9f98445702e15
jaewoo kim <rlawodn0510@yahoo.co.jp>
2022-02-24 18:03:48 +0900

    myBoard second commit

---
+++ myArticle/src/main/java/boardone/BoardDAO.java
@@ -0,0 +1,233 @@
+package boardone;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+import jdk.jshell.spi.ExecutionControl.ExecutionControlException;
+
+public class BoardDAO {
+	private static BoardDAO instance = null;
+
+	private BoardDAO() {
+	}
+
+	public static BoardDAO getInstance() {
+		if (instance == null) {
+			synchronized (BoardDAO.class) {
+				instance = new BoardDAO();
+			}
+		}
+		return instance;
+	}
+
+	// =============================게시판 작업의 기능 하나하나를 메서드로 추가
+	// start========================
+	public void insertArticle(BoardVO article) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int num = article.getNum();
+		int ref = article.getRef();
+		int step = article.getStep();
+		int depth = article.getDepth();
+		int number = 0;
+		String sql = "";
+
+		try {
+			con = ConnUtil.getConnection();
+			ps = con.prepareStatement("select max(num) from board");
+			rs = ps.executeQuery();
+			if (rs.next())
+				number = rs.getInt(1) + 1;
+			else
+				number = 1;
+			if (num != 0) {// 답변글일 경우
+				sql = "update board set step=step+1 where ref=? and step >?";
+				ps = con.prepareStatement(sql);
+				ps.setInt(1, ref);
+				ps.setInt(2, step);
+				ps.executeUpdate();
+				step = step + 1;
+				depth = depth + 1;
+			} else {// 새글일경우
+				ref = number;
+				step = 0;
+				depth = 0;
+			} // 쿼리를 작성
+			sql = "insert into board(num, writer, email, subject, pass, regdate, ref, step, depth, content, ip) values(board_seq.nextval, ?,?,?,?,?,?,?,?,?,?)";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, article.getWriter());
+			ps.setString(2, article.getEmail());
+			ps.setString(3, article.getSubject());
+			ps.setString(4, article.getPass());
+			ps.setTimestamp(5, article.getRegdate());
+			ps.setInt(6, ref);
+			ps.setInt(7, step);
+			ps.setInt(8, depth);
+			ps.setString(9, article.getContent());
+			ps.setString(10, article.getIp());
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			if (ps != null)
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+		}
+	}
+
+	// 첫번째 전체 글의 개수를 가져올 메서드
+	public int getArticleCount() {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int x = 0;
+		try {
+			con = ConnUtil.getConnection();
+			ps = con.prepareStatement("select count(*) from board");
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				x = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			if (ps != null)
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+		}
+		return x;
+	}
+
+	// board table에서 데이터를 가져올 메소드
+	public List<BoardVO> getArticles(/* 수정<1> */) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<BoardVO> articleList = null;
+
+		try {
+			con = ConnUtil.getConnection();
+			/* <수정2> */
+			ps = con.prepareStatement("select * from board order by num desc"); // 수정 <3>
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				articleList = new ArrayList<BoardVO>();// 수정 <4>
+				do {
+					BoardVO article = new BoardVO();
+					article.setNum(rs.getInt("num"));
+					article.setWriter(rs.getString("writer"));
+					article.setEmail(rs.getString("email"));
+					article.setSubject(rs.getString("subject"));
+					article.setPass(rs.getString("pass"));
+					article.setRegdate(rs.getTimestamp("regdate"));
+					article.setReadcount(rs.getInt("readcount"));
+					article.setRef(rs.getInt("ref"));
+					article.setStep(rs.getInt("step"));
+					article.setDepth(rs.getInt("depth"));
+					article.setContent(rs.getString("content"));
+					article.setIp(rs.getString("ip"));
+					articleList.add(article);
+				} while (rs.next());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			if (ps != null)
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+		}
+		return articleList;
+	}
+
+	public BoardVO getArticle(int num) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		BoardVO article = null;
+		try {
+			con = ConnUtil.getConnection();
+			ps = con.prepareStatement("update board set readcount = readcount+1 where num=?");
+			ps.setInt(1, num);
+			ps.executeUpdate();
+			ps = con.prepareStatement("select * from board where num=?");
+			ps.setInt(1, num);
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				article = new BoardVO();
+				article.setNum(rs.getInt("num"));
+				article.setWriter(rs.getString("writer"));
+				article.setEmail(rs.getString("email"));
+				article.setSubject(rs.getString("subject"));
+				article.setPass(rs.getString("pass"));
+				article.setRegdate(rs.getTimestamp("regdate"));
+				article.setReadcount(rs.getInt("readcount"));
+				article.setRef(rs.getInt("ref"));
+				article.setStep(rs.getInt("step"));
+				article.setDepth(rs.getInt("depth"));
+				article.setContent(rs.getString("content"));
+				article.setIp(rs.getString("ip"));
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			if (ps != null)
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+		}
+		return article;
+	}
+
+}
